f916a590ad64ebd5874ae5c8f276f0dc0b1f2908
maltaweel <m.altaweel@ucl.ac.uk>
2025-01-08 16:46:42 +0000

    Add files via upload

--- load_data.py
+++ load_data.py
@@ -1,10 +1,12 @@
 '''
+Data loader and output for SIEM model.
+
 Created on Dec 18, 2024
 
 @author: mark-altaweel
 '''
 
-import sys, os, math
+import sys, os, math, random
 import utm
 from geopy.distance import distance
 from geopy.point import Point
@@ -14,13 +16,79 @@ from settlement import Settlement
 from html5lib.filters import alphabeticalattributes
 from shapely.geometry import mapping, Polygon
 import fiona
+from markov_graph import Graph
 
 from_epsg(25832)
 
 pn=os.path.abspath(__file__)
 pn=pn.split("src")[0]
+
+totalLinkFlow={}
+totalDistance={}
+totalSettlement=[]
+totalFlow={}
+totalAttractiveness={}
+totalLocations={}
+totalPopulation={}
+locations={}
+
+def randomBootstrap(n):
+    a = random.randint(0,100)
     
-def readData(file,alpha,beta):
+    if n > a:
+        return True
+    else:
+        return False
+    
+
+def tallyResults(s):
+    
+    settsOther=s.settlements
+    for i in s.settlements:
+        p1=s.points[i]
+        totalLocations[i]=(p1.x,p1.y)
+            
+        for j in settsOther:
+            if i==j:
+                continue
+      
+            key=str(i)+'-'+str(j)
+            
+            
+            
+            fl=s.linkFlow[key]
+            pop=s.population[i]
+            flo=s.flow[i]
+            atract=s.attractiveness[i]
+            
+            if i not in totalDistance:
+                totalDistance[key]=s.distance[key]
+                
+            if key in totalLinkFlow:
+                fl+=totalLinkFlow[key]
+                
+            if i in totalFlow:
+                flo+=totalFlow[i]
+            
+            if i in totalAttractiveness:
+                atract+=totalAttractiveness[i]
+                
+            if i in totalPopulation:
+                pop+=totalPopulation[i]
+            
+            if i not in totalSettlement:
+                totalSettlement.append(i)
+                
+            if i not in locations:
+                locations[i]=p1
+            
+            totalLinkFlow[key]=fl 
+            totalPopulation[i]=pop 
+            totalFlow[i]=flo
+            totalAttractiveness[i]=atract          
+    
+    
+def readData(file,alpha,beta,randomN):
     
 
     path=os.path.join(pn,'data',file)
@@ -35,13 +103,20 @@ def readData(file,alpha,beta):
     
     s=Settlement()
     
-    n=0
+    numbers=[]
     for i in range(0,len(lat)):
+        if randomBootstrap(randomN) is False:
+            continue
+        else:
+            numbers.append(i)
+    
+    n=0
+    for i in numbers:
         n+=1
         s.alpha=alpha
         s.beta=beta
-        s.points.append(p[i])
-        s.settlements.append(i)
+        s.points[i]=p[i]
+        s.settlements[i]=i
         s.settlement_x[i]=lon[i]
         s.settlement_y[i]=lat[i]
         s.settlement_z[i]=elevation[i]
@@ -51,11 +126,11 @@ def readData(file,alpha,beta):
     
         
         
-    s.totalPopulation=n*s.population[0]  
+    s.totalPopulation=n*100  
     
     return s  
 
-def outputResults(s):
+def outputResults(numberofRuns):
     
     # Define a point feature geometry with one attribute
     schema = {
@@ -69,19 +144,19 @@ def outputResults(s):
     }
     
    
+    path=os.path.join(pn,'output','point_output')
     
-
-    path=os.path.join(pn,'output','output.shp')
     
     path2=os.path.join(pn,'output','line')
     
     with fiona.open(path, 'w', 'ESRI Shapefile',schema) as c:
         
-        for i in s.settlements:
+        for i in totalSettlement:
             c.write({
-                'geometry': mapping(s.points[i]),
-                'properties': {'id': s.settlements[i],'flow':s.flow[i],'attractiveness':s.attractiveness[i],
-                               'population':s.population[i]},
+                'geometry': mapping(locations[i]),
+                'properties': {'id': i,'flow':float(totalFlow[i]/numberofRuns),
+                               'attractiveness':float(totalAttractiveness[i]/numberofRuns),
+                               'population':int(totalPopulation[i]/numberofRuns)},
                 })
             
             
@@ -91,26 +166,34 @@ def outputResults(s):
     
     with fiona.open(path2, 'w', 'ESRI Shapefile',schema2) as c2:        #open a fiona object
         
-            settsOther=s.settlements
-            for i in s.settlements:
+            settsOther=totalSettlement
+            for i in totalSettlement:
             
+                largestFlow=0.0
+                keepKey=''
+                keepXY=[]
                 for j in settsOther:
                     if i==j:
                         continue
                 
-                    p1=s.points[i]
-                    p2=s.points[j]
+                    p1=locations[i]
+                    p2=locations[j]
                 
                     xyList=[(p1.x,p1.y),(p2.x,p2.y)]
                 
                     key=str(i)+'-'+str(j)
                 
-                    fl=s.linkFlow[key]
+                    fl=totalLinkFlow[key]
+                    if (fl>largestFlow) and (totalFlow[j]>totalFlow[i]):
+                        keepKey=key
+                        keepXY=xyList
+                        largestFlow=fl
                 
+                if largestFlow>0.0:
                     c2.write({
                         'geometry': {'type':'LineString',
-                                     'coordinates': xyList},
-                        'properties': {'id': key,'flow':fl},
+                                     'coordinates': keepXY},
+                                     'properties': {'id': keepKey,'flow':float(largestFlow/numberofRuns)},
                         })
                 
             c2.close()
@@ -156,20 +239,34 @@ def calculateDistance(s):
 
 #launch the main
 if __name__ == "__main__":
-    file = str(sys.argv[1])
-    alpha=float(sys.argv[2])
-    beta=float(sys.argv[3])
-    iterations=float(sys.argv[4])
+    file = str(sys.argv[1])  #shapefile settlement data
+    alpha=float(sys.argv[2]) #alpha value
+    beta=float(sys.argv[3])  #beta value
+    iterations=float(sys.argv[4]) #number of iterations
+    randomN=int(sys.argv[5]) #random number of settlements (percentage) to select
+    numberofRuns=int(sys.argv[6]) #number of total runs
     
-    s=readData(file,alpha,beta)
-    calculateDistance(s)
     
-    for i in range(0,int(iterations)):
+    for nn in range(0,numberofRuns):
+        s=readData(file,alpha,beta,randomN)
+        calculateDistance(s)
         s.setFlow()
-        s.calculate_flow()
-        s.adjustAdvantages()
-        s.adjustPopulation()
+        
     
-    outputResults(s)
+        for i in range(0,int(iterations)):
+            s.calculate_flow()
+            s.adjustAdvantages()
+            s.adjustPopulation()
+    
+        tallyResults(s)
+        
+    
+ #  g=Graph()
+ #  g.createGraph(totalSettlement,totalLinkFlow,totalDistance,totalLocations)
+ #  clusters=g.createMarkovCluster(totalLocations)
+ #  g.outputResults(totalSettlement, locations, totalLinkFlow, totalFlow, numberofRuns, clusters)
+    
+    outputResults(numberofRuns)
+    
     
     
